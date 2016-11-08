dc3ce7862a605faa8294dd1053aceea51c5cdcbb
leshary <lesha-ry@yandex.ru>
2016-11-08 19:40:24 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,41 +2,27 @@
 
 using namespace std;
 
-int strange_sum(int* a, int n)
-{
-    int summ = 0;
-    if (n%2==0){
-        int b[n/2];
-        int c[n/2];
-        for (int i = 0; i < n/2; i++){
-        b[i]=a[i];
-        c[i]=a[i+n/2];
-        }
-        summ = strange_sum( b, n/2 ) + strange_sum ( c, n/2 );
-    }
-    else {
-        if ( n != 1 ){
-        int b [(n-1)/2];
-        int c [(n-1)/2];
-        for ( int i = 0; i < (n-1)/2; i++){
-            b[i] = a[i];
-            c[i] = a[i + (n-1)/2];
-        }
-        summ = strange_sum( b, (n-1)/2) + strange_sum( c, (n-1)/2) + a[n-1];
-        }
-        else{
-            summ+=a[n-1];
-        }
-        }
-        return summ;
-    }
+ void delete_duplicates(int* array, int n)
+ {
+     bool flag[n];
+     for (int i=0; i < n; i++)
+        flag[i]=true;
+     for ( int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+           if ( array[i]==array[j] and i!=j)
+           flag[i]=false;
+    for (int i = 0; i < n; i++)
+        if ( flag[i])
+        cout<<array[i]<<" ";
+     }
 int main()
 {
     int n;
-    cin>>n;
-    int* Array = new int [n];
-    for ( int i = 0; i < n ; i++)
-        cin>>Array[i];
-    cout<< strange_sum( Array, n);
+    cin>> n;
+    int* array = new int [n];
+    for (int i = 0; i < n; i++)
+        cin>>array[i];
+    delete_duplicates (array, n);
+    delete [] array;
     return 0;
 }
